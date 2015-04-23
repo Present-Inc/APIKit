990487532da35d979ddd13fb3961897dc7150e22
Justin Makaila <justinmakaila@gmail.com>
2015-04-23 00:34:53 -0400

    Added Result to handle APIRequests

--- APIKit/APIClient.swift
+++ APIKit/APIClient.swift
@@ -8,10 +8,22 @@
 
 import Foundation
 import Alamofire
+import Result
 
-private var acceptableStatusCodeRange = 200..<400
+extension Alamofire.Manager {
+    func request(url: NSURL, route: APIRoute) -> Alamofire.Request {
+        let requestConvertible = APIRequestConvertible(requestUrl: url, route: route)
+        return self.request(requestConvertible)
+    }
+    
+    func request(request: APIRequest) -> Alamofire.Request {
+        return self.request(request.requestConvertible)
+    }
+}
 
 public struct APIClient {
+    public static var acceptableStatusCodeRange: Range<Int> = 200..<400
+    
     public static var cookieStore = NSHTTPCookieStorage.sharedHTTPCookieStorage()
     
     public static var sharedHeaders: [NSObject: AnyObject] = {
@@ -19,26 +31,28 @@ public struct APIClient {
     }()
     
     public static func request(url: NSURL, route: APIRoute) -> Alamofire.Request {
-        let requestConvertible = APIRequestConvertible(requestUrl: url, route: route)
-        return request(requestConvertible)
+        return Manager.sharedInstance.request(url, route: route)
     }
     
     public static func request(request: APIRequest) -> Alamofire.Request {
-        return self.request(request.requestConvertible)
+        return Manager.sharedInstance.request(request)
     }
     
     public static func request(requestConvertible: URLRequestConvertible) -> Alamofire.Request {
-        let request = APIClient()
+        let request = Manager.sharedInstance
             .request(requestConvertible)
-            .validate(statusCode: acceptableStatusCodeRange)
+            .validate(statusCode: APIClient.acceptableStatusCodeRange)
         
-        // TODO: Add a logger
         println(request)
         
         return request
     }
     
     public var manager: Alamofire.Manager
+    
+    public init(sessionConfiguration: NSURLSessionConfiguration? = NSURLSessionConfiguration.defaultSessionConfiguration()) {
+        manager = Alamofire.Manager(configuration: sessionConfiguration)
+    }
     
     public init() {
         self.init(headers: APIClient.sharedHeaders, cookiePolicy: .Always)
@@ -50,7 +64,8 @@ public struct APIClient {
         configuration.HTTPCookieAcceptPolicy = cookiePolicy
         configuration.HTTPCookieStorage = APIClient.cookieStore
         
-        manager = Alamofire.Manager(configuration: configuration)
+        self.init(sessionConfiguration: configuration)
+        
     }
     
     public func request(requestConvertible: URLRequestConvertible) -> Alamofire.Request {
@@ -63,56 +78,98 @@ public struct APIClient {
 /**
     Parses JSON using JSONParser
  */
-public func parseJSONResponse<T, P: JSONParser where P.T == T>(json: JSON, parser: P) -> T? {
+public func parseJSONResponse<T, P: JSONParser where P.T == T>(json: JSON, parser: P) -> Result<T, NSError> {
     if parser.isValid(json) {
-        return parser.parseJSON(json)
+        return Result<T, NSError>.success(parser.parseJSON(json))
     }
     
-    return nil
+    let error = NSError(domain: "tv.present.APIKit.parseJSONResponse", code: -1, userInfo: [
+        NSLocalizedDescriptionKey: "JSON did not pass validation"
+    ])
+    
+    return Result.failure(error)
 }
 
 // MARK: - Serializer for Swifty JSON
 
 extension Alamofire.Request {
-    
     /**
-    Adds a handler to be called once the request has finished.
-    
-    :param: completionHandler A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
-    
-    :returns: The request.
+        Adds a handler to be called once the request has finished.
+        
+        :param: completionHandler A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
+        
+        :returns: The request.
     */
     public func responseSwiftyJSON(completionHandler: (NSURLRequest, NSHTTPURLResponse?, JSON, NSError?) -> Void) -> Self {
         return responseSwiftyJSON(queue: nil, options: NSJSONReadingOptions.AllowFragments, completionHandler: completionHandler)
     }
     
     /**
-    Adds a handler to be called once the request has finished.
-    
-    :param: queue The queue on which the completion handler is dispatched.
-    :param: options The JSON serialization reading options. `.AllowFragments` by default.
-    :param: completionHandler A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
-    
-    :returns: The request.
-    */
-    public func responseSwiftyJSON(queue: dispatch_queue_t? = nil, options: NSJSONReadingOptions = .AllowFragments, completionHandler: (NSURLRequest, NSHTTPURLResponse?, JSON, NSError?) -> Void) -> Self {
+        Adds a handler to be called once the request has finished.
         
-        return response(queue: queue, serializer: Request.JSONResponseSerializer(options: options), completionHandler: { (request, response, object, error) -> Void in
-            
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-                
-                let responseJSON = (object == nil) ? JSON.nullJSON : JSON(object!)
-                
-                dispatch_async(queue ?? dispatch_get_main_queue(), {
-                    completionHandler(self.request, self.response, responseJSON, error)
-                })
-            })
-        })
+        :param: queue The queue on which the completion handler is dispatched.
+        :param: options The JSON serialization reading options. `.AllowFragments` by default.
+        :param: completionHandler A closure to be executed once the request has finished. The closure takes 4 arguments: the URL request, the URL response, if one was received, the SwiftyJSON enum, if one could be created from the URL response and data, and any error produced while creating the SwiftyJSON enum.
+        
+        :returns: The request.
+    */
+    public func responseSwiftyJSON(queue: dispatch_queue_t? = dispatch_get_main_queue(), options: NSJSONReadingOptions = .AllowFragments, completionHandler: (NSURLRequest, NSHTTPURLResponse?, JSON, NSError?) -> Void) -> Self {
+        let serializer = Request.JSONResponseSerializer(options: options)
+        return response(
+            queue: queue,
+            serializer: serializer,
+            completionHandler: { (request, response, object, error) -> Void in
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+                    let json = (object == nil) ? JSON.nullJSON : JSON(object!)
+                    dispatch_async(queue ?? dispatch_get_main_queue()) {
+                        completionHandler(self.request, self.response, json, error)
+                    }
+                }
+            }
+        )
+    }
+    
+    public func resultSwiftyJSON(queue: dispatch_queue_t? = dispatch_get_main_queue(), options: NSJSONReadingOptions? = .AllowFragments, completionHandler: (Result<JSON, NSError>) -> Void) -> Self {
+        let serializer = Request.JSONResponseSerializer(options: options!)
+        
+        return response(
+            queue: queue,
+            serializer: serializer,
+            completionHandler: { (request, response, object, error) -> Void in
+                if let object: AnyObject = object {
+                    completionHandler(Result<JSON, NSError>.success(JSON(object)))
+                } else if let error = error {
+                    completionHandler(Result<JSON, NSError>.failure(error))
+                }
+            }
+        )
     }
     
     public func responseJSONParser<T, P: JSONParser where T == P.T>(parser: P, completionHandler: (NSURLRequest, NSHTTPURLResponse?, T?, NSError?) -> Void) -> Self {
-        return responseSwiftyJSON({ request, response, json, error in
-            completionHandler(request, response, parseJSONResponse(json, parser), error)
+        return responseSwiftyJSON({ (request: NSURLRequest, response: NSHTTPURLResponse?, json: JSON, error: NSError?) -> Void in
+            if let error = error {
+                completionHandler(request, response, nil, error)
+            }
+            
+            let result = parseJSONResponse(json, parser)
+            
+            if let object = result.value {
+                completionHandler(request, response, object, nil)
+            } else if let error = result.error {
+                completionHandler(request, response, nil, error)
+            } else {
+                completionHandler(request, response, nil, nil)
+            }
+        })
+    }
+    
+    public func resultJSONParser<T, P: JSONParser where T == P.T>(parser: P, completionHandler: (Result<T, NSError>) -> Void) -> Self {
+        return resultSwiftyJSON(completionHandler: { result in
+            if let value = result.value {
+                completionHandler(parseJSONResponse(value, parser))
+            } else if let error = result.error {
+                completionHandler(Result<T, NSError>.failure(error))
+            }
         })
     }
 }
